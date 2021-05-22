cf44430b83d04b238a508c42d17f212aa61b78b9
jawabuddha <bjawahar@gmail.com>
2021-05-22 15:04:24 -0400

    Installed request module, added API call to cloud.iex.

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
-
+const request = require('request');
 
 // USE SERVER PORT OR 5000;
 const PORT = process.env.PORT || 5000;
+
+// API Key pk_217be3b5db4a4cb8bf400fd4ab1348f5
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_217be3b5db4a4cb8bf400fd4ab1348f5',{json: true},(err, res, body) =>{
+    if(err){return console.log(err)};
+    if(res.statusCode == 200) {
+        console.log(body);
+    };
+});
+
 
 // set handle bars middleware
 app.engine('handlebars', exphbs());
