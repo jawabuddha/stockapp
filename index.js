b2ab24098d28045a32722963ff97b0ff827e1035
jawabuddha <bjawahar@gmail.com>
2021-05-23 09:43:15 -0400

    installed body-parser; added post function and tied with search button

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require('request');
-const HandlebarsI18n = require("handlebars-i18n");
+const HandlebarsI18n = require('handlebars-i18n');
+const bodyParser = require('body-parser');
+// USE SERVER PORT OR 5000;
+const PORT = process.env.PORT || 5000;
 
 HandlebarsI18n.init({
     resources : {
@@ -23,22 +26,19 @@ HandlebarsI18n.init({
         }
     },
     lng : "en",
-    locale: 'en'
+    locales:"en"
 });
 
-HandlebarsI18n.configure([
-    ["en", "PriceFormat", {currency: "USD"}],
-    ["de", "PriceFormat", {currency: "EUR"}]
-]);
+//use body parser middleware
+//app.use(bodyParser.urlencoded({entended : false})); deprecated -- call separately
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
-
-// USE SERVER PORT OR 5000;
-const PORT = process.env.PORT || 5000;
 
 // API Key pk_217be3b5db4a4cb8bf400fd4ab1348f5
 // create a function to request Stock quotes
 function callAPI(finishedAPI) {
-        request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_217be3b5db4a4cb8bf400fd4ab1348f5',{json: true},(err, res, body) =>{
+        request('https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_217be3b5db4a4cb8bf400fd4ab1348f5',{json: true},(err, res, body) =>{
             if(err){return console.log(err)};
             if(res.statusCode == 200) {
                 finishedAPI(body);
@@ -51,17 +51,33 @@ function callAPI(finishedAPI) {
 // set handle bars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 // constants for web page display
 const otherstuff = "Welcome to the Stock Application.";
 
-// Set handlebar routes
+// Set handlebar index GET routes
 app.get('/', function (req, res) {
     callAPI(function(doneAPI) {
             res.render('home', {
             stock: doneAPI,
         });
-    }, "fb")
+    }, "aapl")
+
+        
+});    
+
+// // Set handlebar index GET routes
+app.post('/', function (req, res) {
+    callAPI(function(doneAPI) {
+//            result = req.body.stock_ticker;
+            res.render('home', {
+            stock: doneAPI,
+//            posted_stuff : result
+        });
+    }, "aapl")
 
         
 });    
@@ -78,4 +94,3 @@ app.get('/about.html', function (req, res) {
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => console.log("Server listening on port "+ PORT));
-
